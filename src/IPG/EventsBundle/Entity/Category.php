5f0073739f93c30601fe4538b5a6b833720bcf8e
Max Melnic <mm@peytz.dk>
2014-03-28 20:49:41 +0200

    Create Category entity

---
+++ src/IPG/EventsBundle/Entity/Category.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace IPG\EventsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Category
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="IPG\EventsBundle\Entity\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="categoryName", type="string", length=255)
+     */
+    private $categoryName;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="parentId", type="integer")
+     */
+    private $parentId;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set categoryName
+     *
+     * @param string $categoryName
+     * @return Category
+     */
+    public function setCategoryName($categoryName)
+    {
+        $this->categoryName = $categoryName;
+
+        return $this;
+    }
+
+    /**
+     * Get categoryName
+     *
+     * @return string 
+     */
+    public function getCategoryName()
+    {
+        return $this->categoryName;
+    }
+
+    /**
+     * Set parentId
+     *
+     * @param integer $parentId
+     * @return Category
+     */
+    public function setParentId($parentId)
+    {
+        $this->parentId = $parentId;
+
+        return $this;
+    }
+
+    /**
+     * Get parentId
+     *
+     * @return integer 
+     */
+    public function getParentId()
+    {
+        return $this->parentId;
+    }
+}
