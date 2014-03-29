4a9b0410a7fd4d89498d21404f59317c7b977e23
Max Melnic <mm@peytz.dk>
2014-03-29 02:02:43 +0200

    Create and check if category exist

--- src/IPG/EventsBundle/Controller/CategoryController.php
+++ src/IPG/EventsBundle/Controller/CategoryController.php
@@ -5,6 +5,8 @@ namespace IPG\EventsBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use IPG\EventsBundle\Entity\Category;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends Controller
 {
@@ -21,5 +23,37 @@ class CategoryController extends Controller
             'IPGEventsBundle:Category:index.html.twig',
             array('categories' => $categories)
         );
+    }
+
+    /**
+     * @Route("/category/auto-complete/{name}", name="category_auto_complete")
+     */
+    public function autoCompleteAction($name)
+    {
+        $categories = $this->getDoctrine()
+            ->getRepository('IPGEventsBundle:Category')
+            ->findBy(
+                array('categoryName' => $name)
+            );
+
+        return $this->render(
+            'IPGEventsBundle:Category:index.html.twig',
+            array('categories' => $categories)
+        );
+    }
+
+    /**
+     * @Route("/category/create/{name}", name="categories_create")
+     */
+    public function createAction($name)
+    {
+        $category = new Category();
+        $category->setCategoryName($name);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($category);
+        $em->flush();
+
+        return new Response('New category name: '.$category->getCategoryName());
     }
 }
